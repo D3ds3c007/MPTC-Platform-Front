bd70d4b7b8260cc740eea5dbe53a0c98cb5800c8
Dedsec007 <nasforger42@gmail.com>
2024-10-02 23:28:35 +0300

    popup done

--- src/app/dashboard/administrator/revenue/page.js
+++ src/app/dashboard/administrator/revenue/page.js
@@ -1,11 +1,24 @@
 'use client';
 import {MEmployeeTable} from "@/app/components/ui/EmployeeTable/MEmployeeTable";
 import {MEmployeeDetails} from "@/app/components/ui/EmployeeDetails/MEmployeeDetails";
+import {MAddButton} from "@/app/components/ui/AddButton/MAddButton";
 import { useState, useEffect } from "react";
+import { MPopup } from "@/app/components/ui/Popup/MPopup";
+import { MMultiStepForm } from "@/app/components/ui/MultiStepForm/MMultiStepForm";
 
 
 export default function RevenuPage(){
+    
 const [selectedMatriculeId, setSelectedMatriculeId] = useState("");
+const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+const handleAddButtonClick = () => {
+    setIsPopupVisible(true);
+}
+
+const handleClosePopup = () => {
+    setIsPopupVisible(false);
+}
 
  useEffect(() => {
         console.log(selectedMatriculeId);
@@ -14,10 +27,15 @@ const [selectedMatriculeId, setSelectedMatriculeId] = useState("");
 
     return(
         <>
-            <h1 style={{
+            {/* <h1 style={{
             color : "var(--dark-blue)",
             fontWeight: "600"
-        }}>Employees</h1>
+        }}>Employees</h1> */}
+
+        {isPopupVisible && 
+            <MPopup title="Add Employee" onClose={handleClosePopup}>
+                <MMultiStepForm />
+            </MPopup>}
 
         <div className="row">
             <div className="col-md-5">
@@ -26,9 +44,16 @@ const [selectedMatriculeId, setSelectedMatriculeId] = useState("");
 
             <div className="col-md-7">
                 <MEmployeeDetails />
+                
             </div>
 
+            
+            
+            <MAddButton onClick={handleAddButtonClick} onClose={handleClosePopup}/>
+            
+
         </div>
+      
 
         </>
     )
