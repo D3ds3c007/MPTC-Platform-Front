c9cc6fa199936af27633592fd37ae6fae0433d40
Pik4booh watches you <120628276+pik4booh@users.noreply.github.com>
2024-10-16 16:00:04 +0300

    update chart

--- src/app/components/ui/Chart/MChart.js
+++ src/app/components/ui/Chart/MChart.js
@@ -3,11 +3,11 @@ import styles from'./MChart.module.css'; // Import the CSS for styling
 export function MChart() {
   return (
     <div className={styles["chart-container"]}>
-    <div className={`${styles["chart-container"]} ${styles["bart"]}`} data-value="15">
-        <div className={`${styles["chart-container"]} ${styles["value-label"]}`}>15</div>
-        <div className={`${styles["chart-container"]} ${styles["tooltip"]}`}>AVG: 15,00</div>
-        <div className={`${styles["chart-container"]} ${styles["fill"]}`}></div>
-        <div className={`${styles["chart-container"]} ${styles["label"]}`}>READING</div>
+    <div className={styles["bart"]} data-value="15">
+        <div className={styles["value-label"]}>15</div>
+        <div className={styles["tooltip"]}>AVG: 15,00</div>
+        <div className={styles["fill"]}></div>
+        <div className={styles["label"]}>READING</div>
     </div>
     </div>
   );
