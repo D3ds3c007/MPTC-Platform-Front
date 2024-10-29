4b34f6c42e740c3064e482adf6854537ff2b72d8
Pik4booh watches you <120628276+pik4booh@users.noreply.github.com>
2024-10-29 15:50:02 +0300

    add upload zip file

--- src/app/dashboard/professor/exam/info/page.js
+++ src/app/dashboard/professor/exam/info/page.js
@@ -1,5 +1,9 @@
+'use client';
+
 import { MFolderCard } from "@/app/components/ui/FolderCard/MFolderCard";
 import { MCard } from "@/app/components/ui/Card/MCard";
+import { MUploadZipFile } from "@/app/components/ui/UploadZipFile/MUploadZipFile";
+import { useForm, FormProvider } from 'react-hook-form'; // Import necessary hooks
 
 export default function Info(){
     return(
@@ -22,8 +26,11 @@ export default function Info(){
                               flexWrap: 'wrap',
                               border: '1px solid #CFD4FA',
                               borderRadius: '10px',
+                              padding: '10px'
                             }}>
-                              
+                                <FormProvider>
+                                    <MUploadZipFile/> This  now has form context
+                                </FormProvider>
                             </div>
                         </div>
                     
@@ -31,7 +38,9 @@ export default function Info(){
                 </div>
                 <div className="col-md-4">
                     <MCard title="Actions" >
-                        
+                    <span><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#01F073" class="bi bi-arrow-up-right-circle-fill" viewBox="0 0 16 16">
+            <path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8m5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707z"/>
+          </svg> This is a card</span>
                     </MCard>
                 </div>
             </div>
