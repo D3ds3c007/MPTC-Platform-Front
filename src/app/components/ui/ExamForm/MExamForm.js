52cb46144c36ea3aea9ba836c04d6a0893d3ee8f
Pik4booh watches you <120628276+pik4booh@users.noreply.github.com>
2024-10-31 15:42:03 +0300

    examform integration

--- src/app/components/ui/ExamForm/MExamForm.js
+++ src/app/components/ui/ExamForm/MExamForm.js
@@ -1,51 +1,124 @@
+import { useState } from 'react';
 import styles from'./MExamForm.module.css'; // Import the CSS for styling
 import { MButton } from '../Button/MButton';
+import axios from '@/app/lib/axiosInstance';
 
-export function MExamForm() {
+export function MExamForm({ levels, subjects, periods }) {
+    const [selectedLevel, setSelectedLevel] = useState(null);
+    const [subjectFile, setSubjectFile] = useState(null);
+    const [assetnoteFile, setAssetnoteFile] = useState(null);
+
+    const [subjectFileName, setSubjectFileName] = useState('No file selected'); // State to store subject file name
+    const [assetnoteFileName, setAssetnoteFileName] = useState('No file selected'); // State to store modal answer file name
+
+    const handleLevelSelect = (level) => {
+        setSelectedLevel(level);
+      };
+
+    const handleFileChange = (event, setFile, setFileName) => {
+        const file = event.target.files[0];
+        if (file) {
+            setFile(file);
+            setFileName(file.name);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!selectedLevel) {
+            alert("Please select a learning level.");
+            return;
+        }
+    
+        // Step 1: Upload the files to the server
+        const formData = new FormData();
+        formData.append('Subject', subjectFile); // Match with DTO property name
+        formData.append('Assetnote', assetnoteFile); // Match with DTO property name
+    
+        formData.append('Session', e.target['exam-session'].value);
+        formData.append('SubjectId', e.target['subject'].value);
+        formData.append('DateExam', e.target['exam-date'].value);
+        formData.append('PeriodId', e.target['exam-period'].value);
+        formData.append('LevelId', selectedLevel.idLevel); // Include selected level ID
+
+        // for (const [key, value] of formData.entries()) {
+        //     console.log(`${key}: ${value}`);
+        // }
+        
+        try {
+            // Send the form data directly to your backend
+            const response = axios.post('Exam/create-exam', formData, {
+                headers: {
+                  'Content-Type': 'multipart/form-data',
+                },
+              });
+    
+            if (!response.ok) {
+                throw new Error('File upload failed');
+            }
+    
+            const result = await response.json();
+            console.log('Data sent successfully:', result);
+            alert('Exam created successfully!');
+    
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred. Please try again.');
+        }
+    };
+    
   return (
     <>
     <div className={styles["chart-container"]}>
-        <form className={styles["exam-form"]}>
+        <form className={styles["exam-form"]} onSubmit={handleSubmit}>
 
             <div className={styles["form-group"]}>
                 <label for="subject">Subject of the exam</label>
-                <select id="subject" required>
-                    <option value="ENGLISH">ENGLISH</option>
-                    <option value="MATH">MATH</option>
+                <select id="subject" name="Subject" required>
+                {subjects.map(subject => (
+                    <option key={subject.idSubject} value={subject.idSubject}>{subject.name}</option>
+                ))}
                 </select>
             </div>
 
             <div className={styles["form-group"]}>
                 <label for="exam-date">Date of the exam</label>
                 <div className={styles["input-with-icon"]}>
-                    <input type="date" id="exam-date" required/>
+                    <input type="date" id="exam-date" name="DateExam" required/>
                 </div>
             </div>
 
             <div className={styles["form-group"]}>
                 <label for="exam-period">Period of the exam</label>
-                <select id="exam-period" required>
-                    <option value="OCT 2024 - DEC 2024">OCT 2024 - DEC 2024</option>
+                <select id="exam-period" name="Period" required>
+                {periods.map(period => (
+                    <option key={period.idPeriod} value={period.idPeriod}>{period.name}</option>
+                ))}
                 </select>
             </div>
 
             <div className={styles["form-group"]}>
                 <label for="exam-session">Session of the exam</label>
-                <select id="exam-session" required>
-                    <option value="TERM 1">TERM 1</option>
-                    <option value="TERM 2">TERM 2</option>
+                <select id="exam-session" name="Session" required>
+                    <option value="1">TERM 1</option>
+                    <option value="2">TERM 2</option>
+                    <option value="3">FINAL</option>
                 </select>
             </div>
 
             <div className={styles["form-group"]}>
                 <label>Learning level</label>
                 <div className={styles["learning-levels"]}>
-                    <button type="button" className={[styles.level, styles.active].join(' ')}>A1</button>
-                    <button type="button" className={styles["level"]}>A2</button>
-                    <button type="button" className={styles["level"]}>B1</button>
-                    <button type="button" className={styles["level"]}>B2</button>
-                    <button type="button" className={styles["level"]}>C1</button>
-                    <button type="button" className={styles["level"]}>C2</button>
+                        {levels.map(level => (
+                            <button
+                                key={level.idLevel}
+                                type="button"
+                                className={`${styles["level"]} ${selectedLevel === level ? styles["active"] : ""}`} // Use 'active' class
+                                onClick={() => handleLevelSelect(level)}
+                            >
+                                {level.name}
+                            </button>
+                        ))}
                 </div>
             </div>
 
@@ -54,11 +127,21 @@ export function MExamForm() {
             <div className={styles["form-group"]}>
                 <label for="subject-file">Add Subject exam</label>
                 <div className={styles["file-upload"]}>
-                    <input type="file" id="subject-file" accept=".pdf" required/>
+                    <input 
+                        type="file" 
+                        id="subject-file" 
+                        name="UriPath" 
+                        accept=".pdf" 
+                        required 
+                        onChange={(e) => handleFileChange(e, setSubjectFile, setSubjectFileName)} 
+                    />
+                    
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#15004F" class="bi bi-upload" viewBox="0 0 16 16" >
                         <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
                         <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
                     </svg>
+                    {subjectFileName && <span className={styles["file-name"]}>{subjectFileName}</span>} {/* Display file name */}
+                    
                 </div>
             </div>
             
@@ -66,11 +149,21 @@ export function MExamForm() {
             <div className={styles["form-group"]}>
                 <label for="modal-answer-file">Add corresponding Asset Note</label>
                 <div className={styles["file-upload"]}>
-                    <input type="file" id="modal-answer-file" accept=".pdf" required/>
+                    <input 
+                        type="file" 
+                        id="modal-answer-file" 
+                        name="UriPathAssetNote" 
+                        accept=".pdf" 
+                        required 
+                        onChange={(e) => handleFileChange(e, setAssetnoteFile, setAssetnoteFileName)} 
+                    />
+
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#15004F" class="bi bi-upload" viewBox="0 0 16 16" >
                         <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
                         <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
                     </svg>
+                    {assetnoteFileName && <span className={styles["file-name"]}>{assetnoteFileName}</span>} {/* Display file name */}
+
                 </div>
             </div>
 
--- src/app/dashboard/professor/exam/create/page.js
+++ src/app/dashboard/professor/exam/create/page.js
@@ -1,20 +1,54 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { MCard } from "@/app/components/ui/Card/MCard";
 import { MExamForm } from "@/app/components/ui/ExamForm/MExamForm";
+import { MLoading } from '@/app/components/ui/Loading/MLoading';
 
 export default function Create()
 {
+  const [levels, setLevels] = useState([]);
+  const [subjects, setSubjects] = useState([]);
+  const [periods, setPeriods] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:5193/api/v1/Data/exam-data'); // Update the URL if needed
+        const data = await response.json();
+
+        console.log("Data fetched:", data);
+
+        // Assuming data structure matches your controller's output
+        setLevels(data[0]);
+        setSubjects(data[1]);
+        setPeriods(data[2]);
+      } catch (error) {
+        console.error("Failed to fetch data:", error);
+      } finally {
+        setLoading(false); // Set loading to false after fetching data
+      }
+    }
+    fetchData();
+  }, []);
+  
     return(
       <div className="col-md-12">
         <MCard title="Add Exam Form">   
-          <div style={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-          }}>
-              <MExamForm></MExamForm>
-          </div>
+          {loading ? (
+            <MLoading></MLoading>
+            // Display loading indicator while data is being fetched
+          ) : (
+            <div style={{
+              display: 'flex',
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+            }}>
+              <MExamForm levels={levels} subjects={subjects} periods={periods} />
+            </div>
+          )}
         </MCard>
-        
       </div>
     )
 }
