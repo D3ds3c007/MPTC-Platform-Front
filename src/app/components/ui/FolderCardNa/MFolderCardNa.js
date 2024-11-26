e5c4810b7938bf050e29c04ce45909fa685151bc
NantenainaRAKOTONIRINA <rakotonirinanantenaina21@gmail.com>
2024-11-26 16:18:16 +0300

    mfolderNA

--- src/app/components/ui/FolderCardNa/MFolderCardNa.js
+++ src/app/components/ui/FolderCardNa/MFolderCardNa.js
@@ -1,5 +1,8 @@
 "use client";
-
+import Image from 'next/image';
+import telechargements from './telechargements.png';
+import favori from './favori.png';
+import arretez from './arretez.png';
 import React, { useState } from 'react';
 import styles from './MFolderCardNa.module.css';
 
@@ -21,6 +24,7 @@ export function MFolderCardNa({ level = "A1", variant = 'primary', title = "Exam
           <p>Session : {session}</p>
           <h6>{subtitle}</h6>
         </div>
+        <div className={styles["Bouton"]}>
         <div className={`${styles["three-dot-button"]} ${styles[variant]}`} onClick={togglePopup}>
           <div className={`${styles["dots"]} ${styles[variant]}`}>
             <span></span>
@@ -28,18 +32,47 @@ export function MFolderCardNa({ level = "A1", variant = 'primary', title = "Exam
             <span></span>
           </div>
         </div>
+        </div>
+       
       </div>
       <div className={styles["corner-icon"]}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="#01F073" className="bi bi-arrow-up-right-circle-fill" viewBox="0 0 16 16">
-          <path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8m5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707z" />
-        </svg>
-      </div>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="50"
+    height="50"
+    fill="#01F073"
+    className="bi bi-arrow-up-right-circle-fill"
+    viewBox="0 0 16 16"
+  >
+    <path d="M0 8a8 8 0 1 0 16 0A8 8 0 0 0 0 8m5.904 2.803a.5.5 0 1 1-.707-.707L9.293 6H6.525a.5.5 0 1 1 0-1H10.5a.5.5 0 0 1 .5.5v3.975a.5.5 0 0 1-1 0V6.707z" />
+  </svg>
+</div>
+
       {/* Popup */}
       {showPopup && (
         <div className={styles["popup"]}>
-          <button className={styles["popup-item"]}>📥 Télécharger</button>
-          <button className={styles["popup-item"]}>❤️ Favoris</button>
-          <button className={styles["popup-item"]}>🚫 Signaler</button>
+          <button className={styles["popup-item"]}>
+            <Image
+          src={telechargements}
+          alt="telechargements"
+          width={25}
+          height={25}
+          className={styles["icon"]}
+        /> Télécharger</button>
+          <button className={styles["popup-item"]}><Image
+          src={favori}
+          alt="favori"
+          width={25}
+          height={25}
+          className={styles["icon"]}
+        />Favoris</button>
+          <button className={styles["popup-item"]}><Image
+          src={arretez}
+          alt="arretez"
+          width={25}
+          height={25}
+          className={styles["icon"]}
+        /> Signaler</button>
         </div>
       )}
     </div>
