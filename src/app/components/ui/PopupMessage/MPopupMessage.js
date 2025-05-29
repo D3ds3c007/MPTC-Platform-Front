acf6e36304184f4c008d095f29c5b112183008fd
RAKOTONIRINA Nantenaina Larissa <152494728+NantenainaRAKOTONIRINA@users.noreply.github.com>
2025-05-30 01:34:50 +0200

    add popup message

---
+++ src/app/components/ui/PopupMessage/MPopupMessage.js
@@ -0,0 +1,41 @@
+// components/PopupMessage.js
+import { useState, useEffect } from "react";
+import styles from "./MPopupMessage.module.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+const MPopupMessage = ({ type = "success", title, message, isVisible, onClose }) => {
+    useEffect(() => {
+      if (isVisible) {
+        console.log("Hello visible");
+        const timer = setTimeout(() => {
+          onClose();
+        }, 3000); // Auto-close after 3 seconds
+        return () => clearTimeout(timer);
+      }
+    }, [isVisible, onClose]);
+  
+    return (
+      <div
+        className={`${styles.popup} ${
+          type === "success" ? styles.success : styles.error
+        } ${isVisible ? styles.show : ""}`}
+      >
+        <button className={styles.close} onClick={onClose}>
+          <i className="bi bi-x-circle"></i>
+        </button>
+        <div className={styles.content}>
+          <span className={styles.icon}>
+            <i className={`bi ${type === "success" ? "bi-check-circle" : "bi-exclamation-circle"}`}></i>
+          </span>
+          <div className={styles.text}>
+            <h4>{title}</h4>
+            <p>{message}</p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+
+export default MPopupMessage;
